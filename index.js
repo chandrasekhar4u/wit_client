ed9b1298b1ddae79e8fdf1c9f458cc4d0dbef464
chandrasekhar4u-admin <chandu624@gmail.com>
2016-08-22 00:56:51 +0530

    FB CHANGES ARE UPDATED

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const bodyParser = require('body-parser');
 //initializing response object.
 var responseObj = {};
 // Webserver parameter
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
+var SERVER_IP_ADDRESS = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
 //import required dependencies.
 const crypto = require('crypto');
 const fetch = require('node-fetch');
@@ -36,7 +37,8 @@ try {
 }
 // Starting our webserver and putting it all together
 app.set('port', PORT);
-app.listen(app.get('port'));
+app.set('ipAddress', SERVER_IP_ADDRESS);
+app.listen(app.get('port'), app.get('ipAddress') );
 app.use(bodyParser.json());
 
 console.log("Server is wating for you @" + PORT);
@@ -53,23 +55,36 @@ const firstEntityValue = (entities, entity) => {
   return typeof val === 'object' ? val.value : val;
 };
 
-//initializing constants.
+
+// Our bot actions
 const actions = {
-  send(request, response) {
-    const {sessionId, context, entities} = request;
-    const {text, quickreplies} = response;
-	return new Promise(function(resolve, reject) {
-      console.log('sending...', JSON.stringify(response));
-	  let resl = resolve();
-	  //assigning response data.
-	  responseObj.sessionId = sessionId;
-	  responseObj.context = context;
-	  responseObj.entities = entities;
-	  responseObj.text = text;
-	  responseObj.quickreplies = quickreplies;
-      return resl;
-    });
+  send({sessionId}, {text}) {
+    // Our bot has something to say!
+    // Let's retrieve the Facebook user whose session belongs to
+    const recipientId = sessions[sessionId].fbid;
+    if (recipientId) {
+      // Yay, we found our recipient!
+      // Let's forward our bot response to her.
+      // We return a promise to let our bot know when we're done sending
+      return fbMessage(recipientId, text)
+      .then(() => null)
+      .catch((err) => {
+        console.error(
+          'Oops! An error occurred while forwarding the response to',
+          recipientId,
+          ':',
+          err.stack || err
+        );
+      });
+    } else {
+      console.error('Oops! Couldn\'t find user for session:', sessionId);
+      // Giving the wheel back to our bot
+      return Promise.resolve()
+    }
   },
+  // You should implement your custom actions here
+  // See https://wit.ai/docs/quickstart
+  
   //custom action called from wit.ai
   ['getCreditCardDetails']({context, entities}) {
 	console.log('entities custom action: '+JSON.stringify(entities));
@@ -82,6 +97,7 @@ const actions = {
     });
   },
 };
+
 
 //find or create session object (ISO date).
 const sessions = {};
@@ -102,6 +118,24 @@ const findOrCreateSession = (sid) => {
   return sessionId;
 };
 
+
+const findOrCreateFBSession = (fbid) => {
+  let sessionId;
+  // Let's see if we already have a session for the user fbid
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].fbid === fbid) {
+      // Yep, got it!
+      sessionId = k;
+    }
+  });
+  if (!sessionId) {
+    // No session found for user fbid, let's create a new one
+    sessionId = new Date().toISOString();
+    sessions[sessionId] = {fbid: fbid, context: {}};
+  }
+  return sessionId;
+};
+
 //TODO: move this to common static content handling.
 //allow static content to access from application.
 app.get('/botTest.html', function(request, response, next){
@@ -114,30 +148,93 @@ app.get('/spinner.gif', function(request, response, next){
     response.sendFile(path.join(__dirname+'/spinner.gif'));
 });
 
+// another example post service
+app.post('/webhook', (req, res, next) => {
+  res.send('"Only those who will risk going too far can possibly find out how far one can go." - Chandra');
+  next();
+});
+
+//::::::::::::::::::::::::::::::::::::::::::::
+//FB Messenger Code
+//::::::::::::::::::::::::::::::::::::::::::::
+
+
+// ----------------------------------------------------------------------------
+// Messenger API specific code
+
+// See the Send API reference
+// https://developers.facebook.com/docs/messenger-platform/send-api-reference
+
+const fbMessage = (id, text) => {
+  const body = JSON.stringify({
+    recipient: { id },
+    message: { text },
+  });
+  const qs = 'access_token=' + encodeURIComponent('ACCESS_TOKEN');
+  return fetch('https://graph.facebook.com/me/messages?' + qs, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body,
+  })
+  .then(rsp => rsp.json())
+  .then(json => {
+    if (json.error && json.error.message) {
+      throw new Error(json.error.message);
+    }
+    return json;
+  });
+};
+
+
+app.use(({method, url}, rsp, next) => {
+  rsp.on('finish', () => {
+    console.log(`${rsp.statusCode} ${method} ${url}`);
+  });
+  next();
+});
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+
+// Webhook setup
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === 'VERIFY_TOKEN') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(400);
+  }
+});
 
 // Message handler
-app.post('/callwit', (req, res) => {
+app.post('/webhook', (req, res) => {
   // Parse the Messenger payload
   // See the Webhook reference
   // https://developers.facebook.com/docs/messenger-platform/webhook-reference
   const data = req.body;
-  const wit = new Wit({accessToken: 'W47X2VGODXS4P22XQHCPHGD2X7RDQTY7', actions, logger: new log.Logger(log.INFO)});
-
+  const wit = new Wit({accessToken: 'WIT_TOKEN', actions, logger: new log.Logger(log.INFO)});
+  
+  if (data.object === 'page') {
     data.entry.forEach(entry => {
       entry.messaging.forEach(event => {
         if (event.message) {
           // Yay! We got a new message!
+          // We retrieve the Facebook user ID of the sender
+          const sender = event.sender.id;
+
           // We retrieve the user's current session, or create one if it doesn't exist
           // This is needed for our bot to figure out the conversation history
-          const sessionId = findOrCreateSession();
+          const sessionId = findOrCreateFBSession(sender);
 
           // We retrieve the message content
           const {text, attachments} = event.message;
 
           if (attachments) {
             // We received an attachment
+            // Let's reply with an automatic message
+            fbMessage(sender, 'Sorry I can only process text messages for now.')
+            .catch(console.error);
           } else if (text) {
             // We received a text message
+
             // Let's forward the message to the Wit.ai Bot Engine
             // This will run all actions until our bot has nothing left to do
             wit.runActions(
@@ -148,10 +245,16 @@ app.post('/callwit', (req, res) => {
               // Our bot did everything it has to do.
               // Now it's waiting for further messages to proceed.
               console.log('Waiting for next user messages');
+
+              // Based on the session state, you might want to reset the session.
+              // This depends heavily on the business logic of your bot.
+              // Example:
+              // if (context['done']) {
+              //   delete sessions[sessionId];
+              // }
+
               // Updating the user's current session state
               sessions[sessionId].context = context;
-			  //console.log('final msg: '+JSON.stringify(responseObj));
-		      res.send(responseObj)
             })
             .catch((err) => {
               console.error('Oops! Got an error from Wit: ', err.stack || err);
@@ -162,17 +265,36 @@ app.post('/callwit', (req, res) => {
         }
       });
     });
-  //res.sendStatus(200);
+  }
+  res.sendStatus(200);
 });
 
-// another example post service
-app.post('/webhook', (req, res, next) => {
-  res.send('"Only those who will risk going too far can possibly find out how far one can go." - Chandra');
-  next();
-});
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
 
-// Webhook get method sends status 400
-app.get('/webhook', (req, res, next) => {
-    res.sendStatus(400);
-	next();
-});
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', 'APP_SECRET')
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
